bf4d9835f8db297a5b5d8db31fcc8c01cf4d244c
Christian Romo <christianromo811@gmail.com>
2024-03-23 15:58:38 -0700

    product.py

---
+++ product.py
@@ -0,0 +1,7 @@
+# Class is a blueprint and all the instructions
+class Product:
+
+    def __init__(self, name, price, category):
+        self.name = name
+        self.price = price
+        self.category = category
